7c2740fc52e4e09277faf85adca098794d5c642f
nizibone21 <nizibonemakeleni@gmail.com>
2023-09-15 13:36:01 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 /**
  * print_numbers - func checks for checks for a digit (0 through 9).
  *
- *Return: A lways 0.
+ * Return: Always 0.
  */
 void print_numbers(void)
-{	
+{
 	int c;
 
-	for (c =48; c < 58; c++) 
+	for (c = 48; c < 58; c++)
 	{
 		_putchar(c);
-	}
 
-
+	}		
 	_putchar('\n');
 }
